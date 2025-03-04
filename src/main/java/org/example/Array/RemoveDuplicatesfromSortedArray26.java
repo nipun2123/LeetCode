99757a7905a76d3772bf3b7578566ec3c7e9a9c6
nipun2123 <nipun.jayasanka10@gmail.com>
2025-03-04 13:47:18 +0000

    26:RemoveDuplicatesfromSortedArray Done

---
+++ src/main/java/org/example/Array/RemoveDuplicatesfromSortedArray26.java
@@ -0,0 +1,51 @@
+package org.example.Array;
+
+public class RemoveDuplicatesfromSortedArray26 {
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+// Difficulty: Easy
+
+//Problem
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+// The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+// The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+// Example 1:
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+
+// Example 2:
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+// Solution
+// I have used the two pointer approach for this.
+// It loop through the array starting from first index. First pointer starts at 0. Second pointer starts at 1.
+// When each pointer's value are not equal, replace the first pointer+1 value with second pointer value. It means add the next highest value to the next position and increment the number of unique value count(k).
+// If each pointer's value are equal, keep looping.
+
+// Time complexity - O(n)
+// It loop through each element of the array once.
+
+// Space complexity - O(1)
+// It used a constant space because it does not depend on the size of the input array.
+
+    public int removeDuplicates(int[] nums) {
+
+        int k = 0;
+
+        for(int i =1; i<nums.length; i++){
+            if(nums[k]!=nums[i]){
+                nums[k+1] = nums[i];
+                k++;
+            }
+
+        }
+        return k+1;
+    }
+}
--- src/main/java/org/example/Array/TwoSum1.java
+++ src/main/java/org/example/Array/TwoSum1.java
@@ -1,35 +1,36 @@
 package org.example.Array;
 
-//https://leetcode.com/problems/two-sum/description/
-//Difficulty: Easy
+// https://leetcode.com/problems/two-sum/description/
+// Difficulty: Easy
 
-//Problem
-//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-//You may assume that each input would have exactly one solution, and you may not use the same element twice.
-//You can return the answer in any order.
+// Problem
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
 
-//Example 1:
-//Input: nums = [2,7,11,15], target = 9
-//Output: [0,1]
-//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-//Example 2:
-//Input: nums = [3,2,4], target = 6
-//Output: [1,2]
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
 
-//Example 3:
-//Input: nums = [3,3], target = 6
-//Output: [0,1]
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
 
 
-//Solution
-//I have used the Brute Force approach for this. It loop through each combination of elements and return if find both value equal to the target.
+// Solution
+// I have used the Brute Force approach for this.
+// It loop through each combination of elements and return if find both value equal to the target.
 
-//Time complexity - O(n2)
-//It loop through each element twice to find the target value.
+// Time complexity - O(n2)
+// It loop through each element of the array twice to find the target value.
 
-//Space complexity - O(1)
-//It used a constant space because it does not depend on the size of the input array.
+// Space complexity - O(1)
+// It used a constant space because it does not depend on the size of the input array.
 
 public class TwoSum1 {
     public int[] twoSum(int[] nums, int target) {
